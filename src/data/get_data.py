0022dffbcefecb79c78dcd0be81dc6a87a68406c
kinger28 <kinger@rhrk.uni-kl.de>
2020-09-14 13:15:38 +0200

    Fixed issue with git cloning the John Hopkins dataset

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -3,9 +3,6 @@ import subprocess
 import os
 
 import pandas as pd
-import numpy as np
-
-from datetime import datetime
 
 import requests
 import json
@@ -14,12 +11,24 @@ def get_johns_hopkins():
     ''' Get data by a git pull request, the source code has to be pulled first
         Result is stored in the predifined csv structure
     '''
-    git_pull = subprocess.Popen( "git pull" ,
+    print('Checking if John Hopkins data repository exists')
+    if os.path.exists('data/raw/COVID-19/'):
+        print('Repository Exists: Fetch the latest data from repository')
+        git_pull = subprocess.Popen( "git pull" ,
                          cwd = os.path.dirname('data/raw/COVID-19/' ),
                          shell = True,
                          stdout = subprocess.PIPE,
                          stderr = subprocess.PIPE )
-    (out, error) = git_pull.communicate()
+        (out, error) = git_pull.communicate()
+    else:
+        print('Repository not present. Fectch the entire repository')
+        git_pull = subprocess.Popen( "git clone https://github.com/CSSEGISandData/COVID-19.git" ,
+                         cwd = os.path.dirname('data/raw/' ),
+                         shell = True,
+                         stdout = subprocess.PIPE,
+                         stderr = subprocess.PIPE )
+        (out, error) = git_pull.communicate()
+    
 
 
     print("Error : " + str(error))
